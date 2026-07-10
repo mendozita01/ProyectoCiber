50bc8790965c8e5aa24e0ce0463d875569d216e9
Andyvmp780 <166755938+mendozita01@users.noreply.github.com>
2026-07-10 16:50:10 -0400

    creando tabla de usuarios y tickets

--- database/setup.sql
+++ database/setup.sql
@@ -1,1 +1,17 @@
+CREATE DATABASE technova_db;
+USE technova_db;
 
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tickets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ip_reportada VARCHAR(50),
+    codigo_diagnostico VARCHAR(255)
+);
+
+INSERT INTO usuarios (username, password_hash) 
+VALUES ('admin', MD5('secreto123'));
